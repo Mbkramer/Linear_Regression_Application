41f2d96392827220bf1ee23511d0bf0fba51700f
mbkramer <maxbkramers@gmail.com>
2024-11-08 15:36:07 -0600

    Used additional car data like heght, weight, length, engine power, fuel tank capaciy, number of seats, but did not help much. Introduced another potential oppuruntiy as not all cars are in the same country

--- vehicle_pricing_regression.py
+++ vehicle_pricing_regression.py
@@ -16,61 +16,79 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn import datasets
 
-df = pd.read_csv('Datasets/CAR DETAILS CLEANED FOR REGRESSION ANALYSIS.csv')
-
-print(df.head())
-print(df.info())
-print(df.describe())
-
-#exploratory data analysis
-# sns linear model plot 
-sns.lmplot(x='year',
-           y='selling_price',
-           data = df,
-           scatter_kws={'alpha':0.3})
-plt.show()
-
-sns.lmplot(x='km_driven',
-           y='selling_price',
-           data = df,
-           scatter_kws={'alpha':0.3})
-plt.show()
-
-sns.lmplot(x='Number of Owners',
-           y='selling_price',
-           data = df,
-           scatter_kws={'alpha':0.3})
-plt.show()
+og_df = pd.read_csv('Datasets/CAR DETAILS CLEANED FOR REGRESSION ANALYSIS.csv')
 
 # split data into training and testing set. Target variable is Yearly amount spent what we want to predict.  
-X = df[['year', 'km_driven', 'Automatic', 'Number of Owners']] # Capital matrix
-y = df['selling_price'] #Single variable 
+X_orig = og_df[['year', 'km_driven', 'Automatic', 'Number of Owners']] # Capital matrix
+y_orig = og_df['selling_price'] #Single variable 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_state=42)
+X_og_train, X_og_test, y_og_train, y_og_test = train_test_split(X_orig, y_orig, test_size=.25, random_state=42)
 
 # Training the model
 lm = LinearRegression()
-lm.fit(X_train, y_train)
+lm.fit(X_og_train, y_og_train)
 
 # Multiple coefficient for each varriable
-cdf = pd.DataFrame(lm.coef_, X.columns, columns=['Coef'])
-print(cdf)
+cog_df = pd.DataFrame(lm.coef_, X_orig.columns, columns=['Coef'])
+print(cog_df)
 
 # Predictions -> what does the algo think the yearly amount spent will be based on the columns given
-predictions = lm.predict(X_test)
+predictions = lm.predict(X_og_test)
 
 # Plot these values against the actual values to see how close they are.. perfect would be a 1x1 straight line
-sns.scatterplot(x=predictions, y=y_test)
+sns.scatterplot(x=predictions, y=y_og_test)
 plt.xlabel("Predictions")
 plt.title("Evaluation of Linear Regression Model")
 plt.show()
 
-print("Mean Absolute Error: ", mean_absolute_error(y_test, predictions))
-print("Mean Squared Error: ", mean_squared_error(y_test, predictions))
-print("RMSE: ", math.sqrt(mean_squared_error(y_test, predictions)))
+print("Mean Absolute Error: ", mean_absolute_error(y_og_test, predictions))
+print("Mean Squared Error: ", mean_squared_error(y_og_test, predictions))
+print("RMSE: ", math.sqrt(mean_squared_error(y_og_test, predictions)))
 
 # residuals actual values - predictions, test for nomality which is assumed by linear models
-residuals = y_test - predictions 
+residuals = y_og_test - predictions 
+sns.displot(residuals, bins=50, kde=True)
+plt.show()
+
+# probability plot
+stats.probplot(residuals, dist="norm", plot=pylab)
+pylab.show()
+
+##########################
+## Now for enhanced data
+##########################
+
+enhanced_df = pd.read_csv('Datasets/ENHANCED_CAR_DETAILS.csv')
+
+# split data into training and testing set. Target variable is Yearly amount spent what we want to predict.  
+X_en = enhanced_df[['Year', 'Kilometer', 'Number of Owners', 'Engine (CC)', 'Length', 'Width', 'Height', 'Seating Capacity', 'Fuel Tank Capacity']] # Capital matrix
+y_en = enhanced_df['Price'] #Single variable 
+
+X_en_train, X_en_test, y_en_train, y_en_test = train_test_split(X_en, y_en, test_size=.25, random_state=42)
+
+# Training the model
+lm = LinearRegression()
+lm.fit(X_en_train, y_en_train)
+
+# Multiple coefficient for each varriable
+cog_df = pd.DataFrame(lm.coef_, X_en.columns, columns=['Coef'])
+print(cog_df)
+
+# Predictions -> what does the algo think the yearly amount spent will be based on the columns given
+predictions_en = lm.predict(X_en_test)
+
+# Plot these values against the actual values to see how close they are.. perfect would be a 1x1 straight line
+sns.scatterplot(x=predictions_en, y=y_en_test)
+plt.xlabel("Predictions")
+plt.title("Evaluation of Linear Regression Model")
+plt.show()
+
+print("Mean Absolute Error: ", mean_absolute_error(y_en_test, predictions_en))
+print("Mean Squared Error: ", mean_squared_error(y_en_test, predictions_en))
+print("RMSE: ", math.sqrt(mean_squared_error(y_en_test, predictions_en)))
+
+# residuals actual values - predictions, test for nomality which is assumed by linear models
+residuals = y_en_test - predictions_en
 sns.displot(residuals, bins=50, kde=True)
 plt.show()
 
