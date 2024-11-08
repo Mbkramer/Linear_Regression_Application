ce2e59587a7b2607e8e599a9662b54e419191ad3
mbkramer <maxbkramers@gmail.com>
2024-11-08 13:52:19 -0600

    Ignore modification to ecommerce_regression. experiemented with linear regression for car price. Model is not that good and under values vehicles. Likely has something to do with the model not collecting any information based on the make and model, anythin related to collectability or novelty, and the seller type.

--- vehicle_pricing_regression.py
+++ vehicle_pricing_regression.py
@@ -1,12 +1,14 @@
-# Intro to linear regression practice 
-# Date: 11/06/2024  
-# Description: This was built coding along side a tutorial video and is not original work. 
+# linear regression practice with vehicle datasets to predict pricing
+# Date: 11/08/2024  
+# Description: This is independent, but informed from recent guided practice projects 
 
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import math
+import pylab
+import scipy.stats as stats
 
 from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
@@ -14,29 +16,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn import datasets
 
-df = pd.read_csv("Datasets/ecommerce_customers.csv")
+df = pd.read_csv('Datasets/CAR DETAILS CLEANED FOR REGRESSION ANALYSIS.csv')
 
 print(df.head())
 print(df.info())
 print(df.describe())
 
-# Exploratory Data Analysis
-sns.pairplot(df, kind='scatter', plot_kws={'alpha': 0.4})
-plt.show() # shows relationships betwen all variables of the dataset.. strong correlation between duration of membership and yearly amount spent 
-
+#exploratory data analysis
 # sns linear model plot 
-sns.lmplot(x='Length of Membership',
-           y='Yearly Amount Spent',
+sns.lmplot(x='year',
+           y='selling_price',
+           data = df,
+           scatter_kws={'alpha':0.3})
+plt.show()
+
+sns.lmplot(x='km_driven',
+           y='selling_price',
+           data = df,
+           scatter_kws={'alpha':0.3})
+plt.show()
+
+sns.lmplot(x='Number of Owners',
+           y='selling_price',
            data = df,
            scatter_kws={'alpha':0.3})
 plt.show()
 
 # split data into training and testing set. Target variable is Yearly amount spent what we want to predict.  
-X = df[['Avg. Session Length', 'Time on App', 'Time on Website', 'Length of Membership']] # Capital matrix
-y = df['Yearly Amount Spent'] #Single variable 
+X = df[['year', 'km_driven', 'Automatic', 'Number of Owners']] # Capital matrix
+y = df['selling_price'] #Single variable 
 
-# split with a 70/30 split 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_state=42)
 
 # Training the model
 lm = LinearRegression()
@@ -61,5 +71,9 @@ print("RMSE: ", math.sqrt(mean_squared_error(y_test, predictions)))
 
 # residuals actual values - predictions, test for nomality which is assumed by linear models
 residuals = y_test - predictions 
-sns.displot(residuals, bins=15, kde=True)
+sns.displot(residuals, bins=50, kde=True)
 plt.show()
+
+# probability plot
+stats.probplot(residuals, dist="norm", plot=pylab)
+pylab.show()
